2c12bd020e038552c3794d13e4be458f8e0f6b64
TonuFish <ReadOnlySpan@outlook.com>
2025-11-09 23:27:06 +0800

    Add tuple musings.

---
+++ src/SomeBenches.TupleSwapBench/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace SomeBenches.TupleSwapBench;
+
+internal static class Program
+{
+	private static void Main(string[] args)
+	{
+		// dotnet run --project .\src\SomeBenches.TupleSwapBench\ -c Release --filter '*Bench*' --affinity 1
+		// _ = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+	}
+}
---
+++ src/SomeBenches.TupleSwapBench/Swaps.cs
@@ -0,0 +1,49 @@
+﻿namespace SomeBenches.TupleSwapBench;
+
+using System;
+
+internal static class Swaps
+{
+	internal static int LocalTempSwap()
+	{
+		Span<int> numbers = [7, 6, 5];
+		var temp = numbers[0];
+		numbers[0] = numbers[1];
+		numbers[1] = temp;
+		return numbers[2];
+	}
+
+	internal static int LocalTupleSwap()
+	{
+		Span<int> numbers = [7, 6, 5];
+		// Enregisters &numbers[1]
+		(numbers[1], numbers[0]) = (numbers[0], numbers[1]);
+		return numbers[2];
+	}
+
+	internal static int ParamTempSwap(Span<int> numbers)
+	{
+		var temp = numbers[0];
+		numbers[0] = numbers[1];
+		numbers[1] = temp;
+		return numbers[2];
+	}
+
+	internal static int ParamTupleSwap_1_0(Span<int> numbers)
+	{
+		(numbers[1], numbers[0]) = (numbers[0], numbers[1]);
+		return numbers[2];
+	}
+
+	internal static int ParamTupleSwap_0_1(Span<int> numbers)
+	{
+		// Bounds checks 0, then 1
+		(numbers[0], numbers[1]) = (numbers[1], numbers[0]);
+		return numbers[2];
+	}
+
+	internal static void ParamTupleSwap_Many(Span<int> a)
+	{
+		(a[1], a[0], a[4], a[3], a[5], a[6]) = (a[6], a[5], a[3], a[4], a[0], a[1]);
+	}
+}
